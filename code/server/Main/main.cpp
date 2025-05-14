866d22c714899688079cc7562e411616ab3bfabd
Yueq2003 <iop18653346184@hotmail.com>
2025-05-14 22:58:58 +0800

    vibe coding only generates bug

--- code/server/Main/main.cpp
+++ code/server/Main/main.cpp
@@ -6,33 +6,27 @@
  * @version 1.0
  */
 
-#include <crow.h>
 #include "../Config/config.h"
-#include "../Middleware/authMiddleware.h"
-#include "../Routers/authRouter.h"
-#include "../Routers/jobRouter.h"
-#include "../Routers/adminRouter.h"
+#include <crow.h>
+
+
+void init()
+{
+    // 初始化全局配置
+    Config::loadConfig();
+}
 
 int main()
 {
-    // 加载配置
-    Config::loadConfig();
-    
+    init();
+
     // 初始化Crow应用
     crow::SimpleApp app;
-    
-    // 注册中间件
-    app.use(AuthMiddleware::handleAuth);
-    
-    // 注册路由
-    AuthRouter::setupRoutes(app);
-    JobRouter::setupRoutes(app);
-    AdminRouter::setupRoutes(app);
-    
+
     // 启动应用
     app.port(Config::globalConfig["PORT"].i())
-       .multithreaded()
-       .run();
-    
+            .multithreaded()
+            .run();
+
     return 0;
 }
