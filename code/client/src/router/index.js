ce4a50fcd9595071478c6db095dea752e2344a32
Seeking Zhou <zsz2411907@gmail.com>
2025-05-23 20:02:33 +0800

    新增用户界面 -增加 views/layouts/UserLayout.vue -修改 router/index.js

--- code/client/src/router/index.js
+++ code/client/src/router/index.js
@@ -31,6 +31,61 @@ const routes = [
     path: '/jobs/:id',
     name: 'JobDetail',
     component: () => import('../views/jobs/JobDetail.vue'),
+  },
+  // 个人用户路由
+  {
+    path: '/user',
+    component: () => import('../views/layouts/UserLayout.vue'),
+    // children: [
+    //   {
+    //     path: 'dashboard',
+    //     name: 'UserDashboard',
+    //     component: () => import('../views/user/Dashboard.vue'),
+    //     meta: { requiresAuth: true, userType: 'individual' }
+    //   },
+    //   {
+    //     path: 'profile',
+    //     name: 'UserProfile',
+    //     component: () => import('../views/user/Profile.vue'),
+    //     meta: { requiresAuth: true, userType: 'individual' }
+    //   },
+    //   {
+    //     path: 'resumes',
+    //     name: 'UserResumes',
+    //     component: () => import('../views/user/ResumeList.vue'),
+    //     meta: { requiresAuth: true, userType: 'individual' }
+    //   },
+    //   {
+    //     path: 'resume/create',
+    //     name: 'CreateResume',
+    //     component: () => import('../views/user/ResumeForm.vue'),
+    //     meta: { requiresAuth: true, userType: 'individual' }
+    //   },
+    //   {
+    //     path: 'resume/edit/:id',
+    //     name: 'EditResume',
+    //     component: () => import('../views/user/ResumeForm.vue'),
+    //     meta: { requiresAuth: true, userType: 'individual' }
+    //   },
+    //   {
+    //     path: 'resume/view/:id',
+    //     name: 'ViewResume',
+    //     component: () => import('../views/user/ResumeView.vue'),
+    //     meta: { requiresAuth: true, userType: 'individual' }
+    //   },
+    //   {
+    //     path: 'applications',
+    //     name: 'UserApplications',
+    //     component: () => import('../views/user/ApplicationList.vue'),
+    //     meta: { requiresAuth: true, userType: 'individual' }
+    //   },
+    //   {
+    //     path: 'settings',
+    //     name: 'UserSettings',
+    //     component: () => import('../views/user/Settings.vue'),
+    //     meta: { requiresAuth: true, userType: 'individual' }
+    //   }
+    // ]
   }
 ]
 
